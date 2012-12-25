a84bf3486557d62fc56718f8d8daf3532b88b693
gavriguy <gavri.guy@gmail.com>
2012-12-25 18:09:15 +0200

    first commit

---
+++ javascripts/fr.js
@@ -0,0 +1,50 @@
+
+
+angular.module('fr', []).
+  config(function($routeProvider) {
+    $routeProvider.
+      when('/thank-you', {controller: ThankYouCtrl, templateUrl: 'partilas/thank-you.html'}).
+      when('/results', {controller: ResultsCtrl, templateUrl:'partilas/results.html'}).
+      otherwise({redirectTo: '/thank-you'});
+
+    });
+
+
+  function ThankYouCtrl($scope, $location) {
+    $scope.setRoute = function(route) {
+      $location.path(route);
+    }
+    
+  }
+
+  function ResultsCtrl($scope, $location) {
+    $scope.setRoute = function(route) {
+      $location.path(route);
+    }
+    $scope.$on('$viewContentLoaded', readyFunction);
+  }
+
+
+
+  //
+
+
+  (function ($) {
+$.fn.vAlign = function() {
+  return this.each(function(i){
+    var ah = $(this).height();
+    var ph = $(this).parent().height();
+    var mh = (ph - ah) / 2;
+  $(this).css('margin-top', mh);
+  });
+
+};
+})(jQuery);
+ 
+
+
+
+function readyFunction() {
+  $('.answer-text, .answer-image, .friends').vAlign();
+}
+
